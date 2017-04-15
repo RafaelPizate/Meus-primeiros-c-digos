17885d9b67d78a905dc0b72f6adcc22895929f3c
RafaelPizate <rafaelpizate@gmail.com>
2017-04-15 12:54:34 -0300

    Programa Área

---
+++ Programaarea.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+main()
+{
+	int base, altura, area;
+	printf("Digite a base do triangulo: ");
+	scanf ("%d",&base);
+	printf("Digite a altura do triangulo: ");
+	scanf ("%d",&altura);
+	area=(base*altura)/2;
+	printf("a area do triangulo e:%d\n", area);
+	
+system("pause");
+}
